c5215f610d78de0f430a36946aaf83b1cadf64d3
dubeyshiv123 <dubeyshiv0123@gmail.com>
2023-05-31 11:51:08 +0530

    Added flight seats update api

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require("sequelize");
 const CrudRepository = require("./crud-repository");
 const { Flight, Airplane, Airport, City } = require("../models");
+const db = require("../models");
 
 class FlightRepository extends CrudRepository {
   constructor() {
@@ -52,6 +53,19 @@ class FlightRepository extends CrudRepository {
       ],
     });
     return response;
+  }
+
+  async updateRemainingSeats(flightId, seats, decrease = true) {
+    await db.sequelize.query(
+      `SELECT * from Flights WHERE Flights.id = ${flightId} FOR UPDATE;`
+    );
+    const flight = await Flight.findByPk(flightId);
+    if (parseInt(decrease)) {
+      await flight.decrement("totalSeats", { by: seats });
+    } else {
+      await flight.increment("totalSeats", { by: seats });
+    }
+    return flight;
   }
 }
 
--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -89,8 +89,25 @@ async function getFlight(id) {
   }
 }
 
+async function updateSeats(data) {
+  try {
+    const response = await flightRepository.updateRemainingSeats(
+      data.flightId,
+      data.seats,
+      data.decrease
+    );
+    return response;
+  } catch (error) {
+    throw new AppError(
+      "Cannot update data of the flight",
+      StatusCodes.INTERNAL_SERVER_ERROR
+    );
+  }
+}
+
 module.exports = {
   createFlight,
   getAllFlights,
   getFlight,
+  updateSeats,
 };
